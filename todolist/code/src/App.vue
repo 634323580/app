42cd2eccdbd0b94594df98d30289caa99f26cd2c
ChenZhiPeng <634323580@qq.com>
2016-08-19 16:03:22 +0800

    todolist完善

--- todolist/code/src/App.vue
+++ todolist/code/src/App.vue
@@ -3,7 +3,8 @@
     <hello></hello>
     <input type="text" v-model="newItem" v-on:keyUp.enter="addNew" id="newItemInput">
     <ul id="todolist">
-      <li v-for="item in items" v-on:click="toggle(item)" :class="{on: item.isFinished}">
+      <li v-for="(index,item) in items" v-on:click="toggle(item)" :class="{on: item.isFinished}">
+        <span class="delete" v-on:click="deleteFn(index,$event)">×</span>
         {{item.label}}
       </li>
     </ul>
@@ -14,6 +15,14 @@
 import Store from './store'
 import Hello from './components/Hello'
 export default {
+  ready() {
+     this.$http.post('https://api.t4tstudio.com/Center/Login').then((response) => {
+      alert('成功'); 
+      console.log(response) 
+    }, (response) => {
+      alert('失败');
+    })
+  },
   data () {
    return {
       age:'这是什么东西.............',
@@ -25,13 +34,22 @@ export default {
   methods: {
     toggle:function(item){
       item.isFinished = !item.isFinished;
+      
     },
     addNew:function(){
+      if(this.newItem == ""){
+        return;
+      }
       this.items.push({
         label: this.newItem,
         isFinished: false
       })
       this.newItem = null;
+    },
+    deleteFn:function(index,e){
+      e.stopPropagation();
+      this.items.splice(index,1)
+
     }
   },
   watch: {
@@ -96,17 +114,23 @@ body {
   margin-bottom: 2px;
   color: #fff;
   opacity: 0.5;
-  cursor: pointer;
+  position: relative;
 }
 #newItemInput{
   width: 250px;
   height:45px;
-  border-radius: 5px;
   border:2px solid rgba(66, 185,134, .5);
   outline: none;
   font-size: 18px;
   text-align: center;
   background: transparent;
   background-origin: padding-box;
+}
+span.delete{
+  color:red;
+  position: absolute;
+  right: 10px;
+  cursor: pointer;
+  font-size: 24px; 
 }
 </style>
